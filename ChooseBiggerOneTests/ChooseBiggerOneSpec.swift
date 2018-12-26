518a753cc53bd58c82166c0e23b58fd2cceddda2
Bob Lee <fightinglb0521@icloud.com>
2018-12-26 17:20:39 +0800

    modify travis.yml

---
+++ ChooseBiggerOneTests/ChooseBiggerOneSpec.swift
@@ -0,0 +1,21 @@
+//
+//  ChooseBiggerOneTests.swift
+//  ChooseBiggerOneTests
+//
+//  Created by CoderBob on 2018/10/22.
+//  Copyright © 2018 lb. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import ChooseBiggerOne
+
+class ChooseBiggerOneSpec: QuickSpec {
+
+  override func spec() {
+    describe("ChooseBiggerOne Test") {
+      
+    }
+  }
+
+}
